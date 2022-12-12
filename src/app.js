0eebbbd105146af8a2024490298cddabf6511c2f
villafaneCristian <villafane.cristian.ezequiel@gmail.com>
2022-12-12 13:42:35 -0300

    comienzo del index.html

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ const path = require ('path');
 
 const app = express();
 
-
-
 //levantamos el servidor de express
 app.listen(3000,()=>{
     console.log('servidor ok y corriendo en el puerto 3000');
 })
+
+const publicPath = path.resolve(__dirname,'./public');
+app.use(express.static(publicPath));
 
 app.get('/',function(req,res){
     const indexPath = path.resolve(__dirname,'./views/index.html');
